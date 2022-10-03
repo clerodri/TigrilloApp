3d5f7e8f5cfd0c0105a23209dc5c43e1565e792b
Ronaldo Rodriguez Coello <clerodri.fiec.espol.edu.ec>
2022-10-03 13:14:10 -0500

    Segundo_Commit Adding Navigation and Fragments

---
+++ app/src/main/java/com/app/tigrillo/view/DetailProducto.kt
@@ -0,0 +1,27 @@
+package com.app.tigrillo.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.app.tigrillo.R
+import com.app.tigrillo.databinding.FragmentDetailProductoBinding
+
+
+class DetailProducto : Fragment() {
+
+    lateinit var  binding : FragmentDetailProductoBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding  = FragmentDetailProductoBinding.inflate(layoutInflater)
+        return binding.root}
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
